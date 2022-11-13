368eab825a895eb7c8105aaf2b962a7347c12e0d
ss-hoon <lshoon1128@naver.com>
2022-11-13 23:06:08 +0900

    fix: Serialize 오류 수정

--- src/main/java/com/godlife/godlifecommonservice/domain/dto/TermDto.java
+++ src/main/java/com/godlife/godlifecommonservice/domain/dto/TermDto.java
@@ -3,8 +3,12 @@ package com.godlife.godlifecommonservice.domain.dto;
 import com.godlife.godlifecommonservice.domain.entity.Term;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Builder(access = AccessLevel.PRIVATE)
+@Getter
+@Setter
 public class TermDto {
 
     /** 약관 타입 */
@@ -21,6 +25,8 @@ public class TermDto {
 
     /** 약관 Entity-DTO 변환 메소드 */
     public static TermDto of(Term term) {
+        if(term == null)    return null;
+
         return TermDto.builder()
                       .type(term.getType())
                       .version(term.getVersion())
