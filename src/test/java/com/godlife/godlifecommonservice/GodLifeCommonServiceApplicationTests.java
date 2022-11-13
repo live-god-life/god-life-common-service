e173c659fc6e9a30357ba697e13efafcca061ac8
ss-hoon <lshoon1128@naver.com>
2022-11-13 13:11:31 +0900

    init

---
+++ src/test/java/com/godlife/godlifecommonservice/GodLifeCommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godlife.godlifecommonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodLifeCommonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
